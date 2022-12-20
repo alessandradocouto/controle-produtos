01d9060645a5e924ac4d9c4511e768a1cb8ecf6c
alessandradocouto <alessandradocouto@outlook.com>
2022-12-19 23:45:07 -0300

    editando server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
-const PORT = process.env.PORT || 3000;
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
-server.use(router);
-server.listen(PORT, () => {
-  console.log("Server is running");
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('./data/db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use((req, res, next) => {
+ if (isAuthorized(req)) { // add your authorization logic here
+   next() // continue to JSON Server router
+ } else {
+   res.sendStatus(401)
+ }
+})
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
